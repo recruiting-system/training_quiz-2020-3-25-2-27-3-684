4365883cdc646ec96c4b27e86132b86a294ef185
linwenjun <linwenjun@163.com>
2020-03-25 02:27:17 +0000

    first commit

---
+++ src/main/java/Address.java
@@ -0,0 +1,7 @@
+public class Address {
+  private String number;
+  private String country;
+  private String city;
+  private String street;
+
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+
+  private String id;
+  private String referenceNumber;
+  private String name;
+  private String type;
+  private Address address;
+  private String telephone;
+  private String email;
+
+  public Person(String id, String referenceNumber, String name, String type, Address address, String telephone, String email) {
+    this.id = id;
+    this.referenceNumber = referenceNumber;
+    this.name = name;
+    this.type = type;
+    this.address = address;
+    this.telephone = telephone;
+    this.email = email;
+  }
+}
